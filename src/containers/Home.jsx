c3892f68f901d6998fee45da578ac47dc5884962
Lenin-Ramirez <leninmoffin@gmail.com>
2022-01-19 19:36:18 -0600

    corrections button-alert

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
 import Search from '../components/Search';
+import swal from 'sweetalert';
 
 
 
 const Home = () => {
 
+  const HandleClick = () =>{ 
+    swal("A wild Pikachu appeared! What do you want to do?", {
+      buttons: {
+        cancel: "Run away!",
+        catch: {
+          text: "Throw Pokéball!",
+          value: "catch",
+        },
+        defeat: true,
+      },
+    })
+    .then((value) => {
+      switch (value) {
+     
+        case "defeat":
+          swal("Pikachu fainted! You gained 500 XP!");
+          break;
+     
+        case "catch":
+          swal("Gotcha!", "Pikachu was caught!", "success");
+          break;
+     
+        default:
+          swal("Got away safely!");
+      }
+    });
+  }
     return ( 
 
       <>
      
       <Search />
+     
+     <button onClick={()=>HandleClick()}>Click me</button>
      
      
     </>
